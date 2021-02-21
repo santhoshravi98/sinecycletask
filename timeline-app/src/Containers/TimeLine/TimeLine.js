09cceb810c2cbb022f84087a79a7b49710f59b1d
Santhosh <rsanthosh411998@gmail.com>
2021-02-21 10:22:33 +0530

    null check condition

--- timeline-app/src/Containers/TimeLine/TimeLine.js
+++ timeline-app/src/Containers/TimeLine/TimeLine.js
@@ -4,10 +4,18 @@ import Auxillary from '../../HOC/Auxillary/Auxillary'
 // A Componene for holding the Parent Unordered List
 class TimeLine extends Component {
     render() {
+        let dynamicDiv = this.props.children;
+
+        // Case when no Timeline items are provided
+        if(typeof dynamicDiv === "undefined")
+        {
+            dynamicDiv=(<li><h3 style={{textAlign:'center'}}>Please Enter a TimeLineItem !!</h3></li>)
+        }
+        
         return (
             <Auxillary>
                 <ul className="list-timeline list-timeline-label" data-testid="ul-list">
-                    {this.props.children}
+                    {dynamicDiv}
                 </ul>
             </Auxillary>
         )
